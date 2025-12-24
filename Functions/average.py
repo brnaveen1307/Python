41a0775217397d88307572951573b6dd4b9a5a17
brnaveenkumar07 <brnaveenkumar1307@gmail.com>
2025-12-24 18:40:38 +0530

    Done

---
+++ Functions/average.py
@@ -0,0 +1,9 @@
+def calculate_average(numbers):
+    return sum(numbers) / len(numbers)
+
+# Student's test scores
+marks = [78, 85, 74, 80, 90]
+
+average_marks = calculate_average(marks)
+
+print("Average Marks:", average_marks)
