cbd7243f89394a628a7243980df786d20907f62a
brnaveenkumar07 <brnaveenkumar1307@gmail.com>
2025-12-30 18:55:15 +0530

    Done

---
+++ Functions/distance.py
@@ -0,0 +1,10 @@
+def calculate_distance(speed, time):
+    return speed * time
+
+# Real-world values
+speed = 60      # km per hour
+time = 2.5      # hours
+
+distance = calculate_distance(speed, time)
+
+print(f"You traveled {distance} kilometers.")
