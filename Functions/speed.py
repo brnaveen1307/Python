e31199a5e5e1cd24c8a5163d9c3a0ad3e98bc234
brnaveenkumar07 <brnaveenkumar1307@gmail.com>
2025-12-31 12:11:01 +0530

    Done

---
+++ Functions/speed.py
@@ -0,0 +1,9 @@
+def calculate_speed(distance, time):
+    return distance / time
+
+distance = 30      # in kilometers
+time = 0.75        # in hours
+
+speed = calculate_speed(distance, time)
+
+print(f"Average speed: {speed} km/h")
