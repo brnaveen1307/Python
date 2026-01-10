049b6ba0e526e990dab47da201fba6e29861f776
brnaveenkumar07 <brnaveenkumar1307@gmail.com>
2026-01-10 20:08:07 +0530

    Done

---
+++ Functions/max.py
@@ -0,0 +1,9 @@
+def find_max(numbers):
+    return max(numbers)
+
+# Daily maximum temperatures for a week (in Celsius)
+weekly_temperatures = [32, 34, 33, 35, 36, 34, 33]
+
+hottest_temperature = find_max(weekly_temperatures)
+
+print("The highest temperature this week is:", hottest_temperature, "°C")
